573cdc239783e6982026296398715c0f63cc258c
Reynaldy Hardiyanto <33504897+reynaldyh@users.noreply.github.com>
2019-05-07 08:44:01 +0700

    Update face_crop.py

--- face_crop.py
+++ face_crop.py
@@ -9,29 +9,62 @@ Created on Tue May  8 17:56:37 2018
 import cv2
 import os
 import numpy as np
+import random
+import fnmatch
+import argparse
+import time
 
 faceCascade = cv2.CascadeClassifier('/Users/reynaldyhardiyanto/anaconda3/pkgs/opencv-3.3.1-py35hb620dcb_1/share/OpenCV/lbpcascades/lbpcascade_frontalface_improved.xml')
 
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Face Crop",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "--raw_images_dir",
+        type=str,
+        required=True,
+        help='input raw images filepath'
+    )
+    parser.add_argument(
+        "--outpath",
+        type=float,
+        required=True,
+        help='result image filepath'
+    )
+    args = parser.parse_args()
+    return args
 
-for i in range(28):
-   for j in range(3):
-        # Capture frame-by-frame
-        src = '/Users/reynaldyhardiyanto/Desktop/Data Gender/'
-        src = src + str(i+1) + '/00' + str(j+1) + '.png'
-        frame = cv2.imread(src)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-    
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            faceImage = frame[y:y+h,x:x+w]
-        path = "/Users/reynaldyhardiyanto/Desktop/Hasil Data Gender/"+str(i+1)
-        cv2.imwrite(os.path.join(path,str(j+1)+'.jpg'), faceImage)
-cv2.waitKey(0)
-# When everything is done, release the capture
+
+def crop_faces(
+        raw_images_dir,
+        outpath,
+        image_ext_pattern_list=['jpg']):
+    list_images = []
+    # print(val_files)
+    for root, dirs, files in os.walk(raw_images_dir):
+        for filename in files:
+            print(filename)
+            image_path = os.path.join(root, filename)
+            img = cv2.imread(image_path)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+        
+            # Draw a rectangle around the faces
+            for i,face in enumerate(faces):
+                for (x, y, w, h) in face:
+                    faceImage = img[y:y+h,x:x+w]
+                    cv2.imwrite(os.path.join(outpath,filename,str(i)), faceImage)
+            
+                   
+if __name__ == "__main__":
+    args = get_args()
+    raw_images_dir = args.raw_images_dir
+    outpath = args.outpath
+    crop_faces(raw_images_dir,outpath)
